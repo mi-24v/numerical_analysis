2c862706d139709a2552def9184adaa5d1ead2ca
mi-24v <miwpayou0808@gmail.com>
2018-08-03 23:27:03 +0900

    [FIXED]rewrite ordinary differential equations solver

--- ordinary_differential_equations.cpp
+++ ordinary_differential_equations.cpp
@@ -12,45 +12,36 @@
 #include <iterator>
 #include <utility>
 #include <functional>
-#include <unordered_map>
 
 const double EPS = .00000001;
 
 //prototype declaration
 std::vector<std::pair<double, double>> forward_euler_method(std::function<double(double)> func, double start, double end, double step, double cycle);
 
-enum class RKStage{
-	RK1,
-	RK2,
-	RK4
-};
-
 class RunngeKutta{
 public:
-	RunngeKutta(RKStage stage);
+	//RunngeKutta(RKStage stage);
+	RunngeKutta(unsigned int stage, std::vector<std::vector<double>> rk_matrix, std::vector<double> rk_weights, std::vector<double> rk_nodes);
 	~RunngeKutta();
 
 	std::vector<std::pair<double, double>> runge_kutta_method(std::function<double(double, double)> func, double start, double end, double step, double cycle);
 
+	// 1st-stage runnge-kutta(euler method)
+	const static RunngeKutta RK1;
+	//2nd-stage runnge-kutta
+	const static RunngeKutta RK2;
+	//4th-stage runnge-kutta
+	const static RunngeKutta RK4;
+
 private:
-	// enum that determine which table should be use
-	RKStage rk_stage;
+	// stage size.
+	unsigned int rk_stage;
 
 	// actual using coeffs.
-	std::unordered_map<std::string, std::vector<double>> rk_coeffs;
+	std::vector<std::vector<double>> rk_matrix;
+	std::vector<double> rk_weights;
+	std::vector<double> rk_nodes;
 
-	// Butcher Table
-	// key definition
-	const static std::string RK_matrix,
-		RK_weights,
-		RK_nodes;
-	// const static array
-	// 1st-stage runnge-kutta(euler method)
-	const static std::unordered_map<std::string, std::vector<double>> RK1;
-	//2nd-stage runnge-kutta
-	const static std::unordered_map<std::string, std::vector<double>> RK2;
-	//4th-stage runnge-kutta
-	const static std::unordered_map<std::string, std::vector<double>> RK4;
 };
 
 int main(int argc, char const *argv[]){
@@ -64,33 +55,37 @@ int main(int argc, char const *argv[]){
 		return 2.0 * x;
 	};
 
-	const double start = 0.0,
-				end = 10.0,
+	const double start = 1.0,
+				end = 3.0,
 				step = 0.01,
-				cycle = 1.0;
-	RunngeKutta rk2(RKStage::RK2), rk4(RKStage::RK4);
-	std::vector<std::pair<double, double>> euler_values = forward_euler_method(func_ff, start, end, step, cycle);
-	auto rk2_values = rk2.runge_kutta_method(func_fff, start, end, step, cycle);
-	auto rk4_values = rk4.runge_kutta_method(func_fff, start, end, step, cycle);
-	printf("  X\t  Y\t\n");
+				cycle = 0.2;
+	RunngeKutta rk1(RunngeKutta::RK1), rk2(RunngeKutta::RK2), rk4(RunngeKutta::RK4);
+	//std::vector<std::pair<double, double>> euler_values = forward_euler_method(func_ff, start, end, step, cycle);
+	std::vector<std::pair<double, double>> euler_values = rk1.runge_kutta_method(func_f, start, end, step, cycle);
+	auto rk2_values = rk2.runge_kutta_method(func_f, start, end, step, cycle);
+	auto rk4_values = rk4.runge_kutta_method(func_f, start, end, step, cycle);
+	printf("[euler values]\n");
+	printf("  X\t, Y\t\n");
 	for(std::pair<double, double> point : euler_values){
-		printf("%7.4lf %7.4lf\n", point.first, point.second);
+		printf("%7.4lf,%7.4lf\n", point.first, point.second);
 	}
-	printf("  X\t  Y\t\n");
+	printf("[RK2 values]\n");
+	printf("  X\t, Y\t\n");
 	for(std::pair<double, double> point : rk2_values){
-		printf("%7.4lf %7.4lf\n", point.first, point.second);
+		printf("%7.4lf,%7.4lf\n", point.first, point.second);
 	}
-	printf("  X\t  Y\t\n");
+	printf("[RK4 values]\n");
+	printf("  X\t, Y\t\n");
 	for(std::pair<double, double> point : rk4_values){
-		printf("%7.4lf %7.4lf\n", point.first, point.second);
+		printf("%7.4lf,%7.4lf\n", point.first, point.second);
 	}
 
 	return 0;
 }
 
 std::vector<std::pair<double, double>> forward_euler_method(std::function<double(double)> func, double start, double end, double step, double cycle){
-	double x = 0.0,
-			y = 0.0,
+	double x = start,
+			y = start,
 			term = 0.0;
 	std::vector<std::pair<double, double>> values;
 	do{
@@ -105,51 +100,47 @@ std::vector<std::pair<double, double>> forward_euler_method(std::function<double
 	return values;
 }
 
-const std::string RunngeKutta::RK_matrix = "RK_matrix",
-		RunngeKutta::RK_weights = "weights",
-		RunngeKutta::RK_nodes = "nodes";
-
 // 1st-stage runnge-kutta(euler method)
-const std::unordered_map<std::string, std::vector<double>> RunngeKutta::RK1 = {
-		{RK_matrix, {0}},
-		{RK_weights, {1}},
-		{RK_nodes, {0}}
-	};
+const RunngeKutta RunngeKutta::RK1 = RunngeKutta(
+		1,
+		{{0}},//RK matrix
+		{1},//RK weights
+		{0}//RK nodes
+	);
 //2nd-stage runnge-kutta
-const std::unordered_map<std::string, std::vector<double>> RunngeKutta::RK2 = {
-	{RK_matrix, {0, 1}},
-	{RK_weights, {0.5, 0.5}},
-	{RK_nodes, {0, 1}}
-};
+const RunngeKutta RunngeKutta::RK2 = RunngeKutta(
+		2,
+		{{0,0},
+		 {1,0}},
+		{0.5, 0.5},
+		{0, 1}
+	);
 //4th-stage runnge-kutta
-//FIXME maybe invalid..?
-const std::unordered_map<std::string, std::vector<double>> RunngeKutta::RK4 = {
-	{RK_matrix, {0, 0.5, 0.5, 1}},
-	{RK_weights, {1/6, 1/3, 1/3, 1/6}},
-	{RK_nodes, {0, 0.5, 0.5, 1}}
-};
+constexpr double rk4_weight_1 = 1.0/6.0;
+constexpr double rk4_weight_2 = 1.0/3.0;
+const RunngeKutta RunngeKutta::RK4 = RunngeKutta(
+		4,
+		{{0, 0, 0,0},
+		 {0.5,0,0,0},
+		 {0,0.5,0,0},
+		 {0, 0, 1,0}},
+		{rk4_weight_1, rk4_weight_2, rk4_weight_2, rk4_weight_1},
+		{0, 0.5, 0.5, 1}
+	);
 
-RunngeKutta::RunngeKutta(RKStage stage) {
-	// init rk coeffs.
-	switch(stage){
-		case RKStage::RK1:
-			rk_coeffs = RunngeKutta::RK1;
-			break;
-		case RKStage::RK2:
-			rk_coeffs = RunngeKutta::RK2;
-			break;
-		case RKStage::RK4:
-		default:
-			rk_coeffs = RunngeKutta::RK4;
-	}
+RunngeKutta::RunngeKutta(unsigned int stage, std::vector<std::vector<double>> rk_matrix, std::vector<double> rk_weights, std::vector<double> rk_nodes){
+	this->rk_stage = stage;
+	this->rk_matrix = rk_matrix;
+	this->rk_weights = rk_weights;
+	this->rk_nodes = rk_nodes;
 }
 
 RunngeKutta::~RunngeKutta(){
 }
 
 std::vector<std::pair<double, double>> RunngeKutta::runge_kutta_method(std::function<double(double, double)> func, double start, double end, double step, double cycle){
-	double x = 0.0,
-			y = 0.0,
+	double x = start,
+			y = start,
 			term = 0.0;
 	std::vector<double> approximations;
 	std::vector<std::pair<double, double>> values;
@@ -161,17 +152,20 @@ std::vector<std::pair<double, double>> RunngeKutta::runge_kutta_method(std::func
 			values.push_back(point);
 		}
 		// obtain approximations.
-		for(unsigned int i = 0; i < std::min(rk_coeffs[RK_matrix].size(), rk_coeffs[RK_nodes].size()); i++){
-			double y_value =
-				i == 0?
-				y + step * rk_coeffs[RK_matrix][i]:
-				y + step * rk_coeffs[RK_matrix][i] * approximations[i-1];
-			approximations.push_back(func(x+step*rk_coeffs[RK_nodes][i], y_value));
+		for(unsigned int i = 0; i < rk_stage; i++){
+			double approximation_sum = 0;
+			for(unsigned int j = 1; j < i - 1; j++){
+				if(j >= approximations.size())break;
+				approximation_sum += rk_matrix[i][j] * approximations[j];
+			}
+			double x_value = x + step * rk_nodes[i];
+			double y_value = y + step * approximation_sum;
+			approximations.push_back(func(x_value, y_value));
 		}
 		// apply
-		double sum = 0;
-		for(unsigned int i = 0; i < rk_coeffs[RK_weights].size(); i++){
-			sum += step * rk_coeffs[RK_weights][i] * approximations[i];
+		double sum = 0.0;
+		for(unsigned int i = 0; i < rk_stage; i++){
+			sum += step * rk_weights[i] * approximations[i];
 		}
 
 		y += sum;
